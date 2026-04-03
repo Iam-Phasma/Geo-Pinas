14a38e3231044e4f764a5c769e34392d73bf3a86
Iam-Phasma <ricsonjaymadonza@gmail.com>
2026-04-03 10:06:09 +0800

    Restrict CORS to site origin

--- convex/visitors.ts
+++ convex/visitors.ts
@@ -1,13 +1,18 @@
 import { httpAction, internalMutation } from "./_generated/server";
 import { internal } from "./_generated/api";
 
-export const trackVisit = httpAction(async (ctx) => {
-  const count = await ctx.runMutation(internal.visitors.increment);
-  return new Response(JSON.stringify({ count }), {
-    status: 200,
+export const trackVisit = httpAction(async (ctx, request) => {
+  const origin = request.headers.get("Origin") ?? "";
+  const allowed = origin === "https://iam-phasma.github.io";
+  const count = allowed
+    ? await ctx.runMutation(internal.visitors.increment)
+    : null;
+  return new Response(allowed ? JSON.stringify({ count }) : null, {
+    status: allowed ? 200 : 403,
     headers: {
       "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Origin": "https://iam-phasma.github.io",
+      "Access-Control-Allow-Methods": "POST",
     },
   });
 });
